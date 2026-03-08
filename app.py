9f914991ff5a378f5a6b5e75df133db72cf4d605
Sharan <chedulurisharanraju@gmail.com>
2026-03-08 19:20:36 +0530

    added gunicorn for render deployment

--- app.py
+++ app.py
@@ -821,3 +821,6 @@ if __name__ == "__main__":
         from database import init_db
         init_db()
     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
